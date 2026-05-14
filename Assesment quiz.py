e599efd5040a9e25930a3b0675b55e03cafc31df
24johnstont-seahorse <24johnstont@mountainview.school.nz>
2026-05-14 13:18:20 +1200

    Fixed the scoring

--- Assesment quiz.py
+++ Assesment quiz.py
@@ -17,19 +17,28 @@ if player_age <= MIN:
 elif player_age >= MAX:
   print ("You are too old for this quiz. I recommend the Cyber Smart Youth Quiz to ages 14 or higher")
 else:
-  while player_age >= MIN and player_age <= MAX:
     time.sleep (1)
     player_name = input ("Please enter your name (No personal information such as e-mails or phone numbers) ")
     print ("Hello, "+str(player_name) )
     time.sleep (1)
     print ("Question time")
     time.sleep (1)
-    if player_age >= MIN and player_age <= MAX:
+    while guesses < 3:
+     guesses += (1)
      Q1 = input ("You want to join an online gaming site. Which of the following information is okay to post on the site? A:A nickname B:Your real name C:Your e-mail address ").upper ()
+    
     if Q1 == "A":
       print ("Correct!")
       score += 1
       time.sleep (1)
+     
+     
+     
+     
+     
+     
+     
+     
       Q2 = input ("If you see somebody with an offensive user name online, what is the correct response? A: Laugh at it B: Do nothing C: Report it ").upper()
       if Q2 == "C":
        print ("Correct!")
@@ -45,9 +54,9 @@ else:
      print ("Correct!")
      score += 1
      RES = input("Would you like to see your results? Y/N ").upper()
-    if RES == "N":
+     if RES == "N":
        print ("Ok")
-    elif RES == "Y":
+     elif RES == "Y":
          print("Your score is "+str(score))
   
   
