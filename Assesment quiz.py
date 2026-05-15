f2e730b0d50303f89b7ce4f8b19de23f511eed11
24johnstont-seahorse <24johnstont@mountainview.school.nz>
2026-05-15 15:06:54 +1200

    Added the question repeater

--- Assesment quiz.py
+++ Assesment quiz.py
@@ -23,40 +23,45 @@ else:
     time.sleep (1)
     print ("Question time")
     time.sleep (1)
+
+    while guesses < 3:
+      guesses += (1)
+      Q1 = input ("You want to join an online gaming site. Which of the following information is okay to post on the site? A:A nickname B:Your real name C:Your e-mail address ").upper ()
+      if Q1 == "A":
+        print ("Correct!")
+        score += 1
+        time.sleep (1)
+        break
+     
     while guesses < 3:
      guesses += (1)
-     Q1 = input ("You want to join an online gaming site. Which of the following information is okay to post on the site? A:A nickname B:Your real name C:Your e-mail address ").upper ()
-    
-    if Q1 == "A":
-      print ("Correct!")
-      score += 1
-      time.sleep (1)
-     
-     
-     
-     
-     
-     
-     
-     
-      Q2 = input ("If you see somebody with an offensive user name online, what is the correct response? A: Laugh at it B: Do nothing C: Report it ").upper()
-      if Q2 == "C":
+     Q2 = input ("If you see somebody with an offensive user name online, what is the correct response? A: Laugh at it B: Do nothing C: Report it ").upper()
+     if Q2 == "C":
        print ("Correct!")
        score += 1
        time.sleep (1)
-    Q3 = input ("If someone sends something inappropriate to you, what is the best action to do? A:Ingnore it B:Tell a trusted adult C:Send it to your friends ").upper()
-    if Q3 == "B":
+       break
+     
+    while guesses < 3:
+     guesses += (1)
+     Q3 = input ("If someone sends something inappropriate to you, what is the best action to do? A:Ingnore it B:Tell a trusted adult C:Send it to your friends ").upper()
+     if Q3 == "B":
       print ("Correct!")
       score += 1
-    time.sleep (1)
-    Q4 = input ("You lose a game to one of your friends, what is the best way to respond? A:Yell at them B:Rage quit C:Say something positive ").upper()
-    if Q4 == "C":
-     print ("Correct!")
-     score += 1
-     RES = input("Would you like to see your results? Y/N ").upper()
-     if RES == "N":
+     time.sleep (1)
+     break
+
+    while guesses < 3:
+      guesses += (1)
+      Q4 = input ("You lose a game to one of your friends, what is the best way to respond? A:Yell at them B:Rage quit C:Say something positive ").upper()
+      if Q4 == "C":
+       print ("Correct!")
+      score += 1
+      break
+#Make it repeat if input is not Y or N
+    RES = input("Would you like to see your results? Y/N ").upper()
+    if RES == "N":
        print ("Ok")
-     elif RES == "Y":
+    elif RES == "Y":
          print("Your score is "+str(score))
-  
   
